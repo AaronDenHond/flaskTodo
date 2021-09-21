c4c3359294b899a3150f6f8f357d96ef8bb2e166
Aaron Den Hond <aaron.denhond@gmail.com>
2021-09-21 11:56:35 +0200

    delete functionality, some db updates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import sys
 
 from werkzeug.utils import redirect
 
@@ -20,12 +21,14 @@ class Todo(db.Model):
         return '<Task %r' % self.id
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         # value of taskContent is the form input with the name taskcontent
         taskContent = request.form['taskcontent']
-        newTask = Todo(content=taskContent)
+        newTask = Todo(content = taskContent)
+        print(taskContent, file=sys.stderr)
+        
         # good practice to use try catches wherever poss, here it seems to be try
         try:
             db.session.add(newTask)
@@ -33,12 +36,31 @@ def index():
             return redirect('/')
 
         except:
-            return "there's an error adding your task bro"
+            return "there's an error adding your task bro unlucky"
 
     else:
         #query to return tasks in order of date created.  
         tasks = Todo.query.order_by(Todo.dateCreated).all()  
         return render_template("index.html", tasks = tasks)
+
+
+# Delete functionality. We need a diff route first. when going to that route, we want a delete function to trigger. we need to identify what task to delete
+# We want to take smth unique to the task to identify it by, which will be the id of said task/todo.
+# We also need to update the hrefs in index.html.
+
+#the <int:id> is needed here cause if we just route to delete, the url doesnt exist when clicking the link.<> is used to specify a variable for dynamic routing.
+@app.route('/delete/<int:id>')
+def delete(id):
+    todoToDelete = Todo.query.get(id)
+    
+    try:
+        db.session.delete(todoToDelete)
+        db.session.commit()
+        return redirect('/')
+    
+    except:
+        return "there was an issue deleting your todo"
+    
 
 
 # this is so your module doesnt insta run in import
